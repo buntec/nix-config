68cad9aadde72abe218971a6bd955b562e9f2807
Christoph Bunte <christophbunte@gmail.com>
2023-06-25 14:00:11 +0200

    change tmux plugins

--- home/tmux/tmux.nix
+++ home/tmux/tmux.nix
@@ -12,13 +12,6 @@
     escapeTime = 0;
     plugins = with pkgs; [
       tmuxPlugins.resurrect
-      {
-        plugin = tmuxPlugins.continuum;
-        extraConfig = ''
-          set -g @continuum-restore 'on'
-          set -g @continuum-save-interval '15' # minutes
-        '';
-      }
     ];
     extraConfig = ''
       set -g default-command "exec ${pkgs.fish}/bin/fish"
