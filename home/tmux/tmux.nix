f71b50dc025f78b6c8b4abcbaa4d71d5de47d7c6
Christoph Bunte <christophbunte@gmail.com>
2023-09-25 17:19:01 +0200

    update tmux config

--- home/tmux/tmux.nix
+++ home/tmux/tmux.nix
@@ -13,6 +13,11 @@
     plugins = with pkgs; [ tmuxPlugins.resurrect ];
     extraConfig = ''
       set -g default-command "exec ${pkgs.fish}/bin/fish"
+
+      # Set new panes to open in current directory
+      bind c new-window -c "#{pane_current_path}"
+      bind '"' split-window -c "#{pane_current_path}"
+      bind % split-window -h -c "#{pane_current_path}"
     '';
   };
 }
