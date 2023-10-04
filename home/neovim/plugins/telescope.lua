a75043020e31f5f07988406f9416be6b674266d9
Christoph Bunte <christophbunte@gmail.com>
2023-10-04 17:55:31 +0200

    nvim: add keymap

--- home/neovim/plugins/telescope.lua
+++ home/neovim/plugins/telescope.lua
@@ -22,6 +22,7 @@ vim.keymap.set("n", "<leader>lg", builtin.live_grep, { desc = "live grep" })
 vim.keymap.set("n", "<leader>gs", builtin.grep_string, { desc = "grep string" })
 vim.keymap.set("n", "<leader>bu", builtin.buffers, { desc = "buffers" })
 vim.keymap.set("n", "<leader>co", builtin.commands, { desc = "commands" })
+vim.keymap.set("n", "<leader>di", builtin.diagnostics, { desc = "diagnostics" })
 vim.keymap.set("n", "<leader>ht", builtin.help_tags, { desc = "help tags" })
 vim.keymap.set("n", "<leader>ts", builtin.treesitter, { desc = "treesitter" })
 vim.keymap.set("n", "<leader>cs", function() builtin.colorscheme({ enable_preview = true }) end,
