c7d77e5d246c795a13b47bb3169ede187bacfedf
Christoph Bunte <christophbunte@gmail.com>
2023-11-18 21:28:33 +0100

    remove unnecessary fish plugins

--- home/fish/fish.nix
+++ home/fish/fish.nix
@@ -1,30 +1,10 @@
 { pkgs, ... }: {
   programs.fish = {
     enable = true;
-    plugins = [
-      {
-        name = "pure";
-        inherit (pkgs.fishPlugins.pure) src;
-      }
-      {
-        name = "bass";
-        src = pkgs.fetchFromGitHub {
-          owner = "edc";
-          repo = "bass";
-          rev = "50eba266b0d8a952c7230fca1114cbc9fbbdfbd4";
-          sha256 = "0ppmajynpb9l58xbrcnbp41b66g7p0c9l2nlsvyjwk6d16g4p4gy";
-        };
-      }
-      {
-        name = "foreign-env";
-        src = pkgs.fetchFromGitHub {
-          owner = "oh-my-fish";
-          repo = "plugin-foreign-env";
-          rev = "dddd9213272a0ab848d474d0cbde12ad034e65bc";
-          sha256 = "00xqlyl3lffc5l0viin1nyp819wf81fncqyz87jx8ljjdhilmgbs";
-        };
-      }
-    ];
+    plugins = [{
+      name = "pure";
+      inherit (pkgs.fishPlugins.pure) src;
+    }];
 
     interactiveShellInit = ''
       fish_vi_key_bindings
