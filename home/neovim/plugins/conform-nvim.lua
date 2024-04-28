50b0095b4e96a114824aecb5a17aa1140da5fcc8
Christoph Bunte <christophbunte@gmail.com>
2024-04-28 19:28:38 +0200

    use prettier for md files

--- home/neovim/plugins/conform-nvim.lua
+++ home/neovim/plugins/conform-nvim.lua
@@ -1,12 +1,12 @@
 require("conform").setup({
   formatters_by_ft = {
-    --markdown = { "mdformat" }, -- mdformat doesn't play nice with e.g., pandoc markdown
-    css = { { "prettierd", "prettier" }, "stylelint" },
     cpp = { "clang_format" },
+    css = { { "prettierd", "prettier" }, "stylelint" },
     html = { { "prettierd", "prettier" } },
     javascript = { { "prettierd", "prettier" } },
     json = { "jq" },
     lua = { "stylua" },
+    markdown = { { "prettierd", "prettier" } },
     python = { "ruff_format", "ruff_fix" },
     typescript = { { "prettierd", "prettier" } },
     yaml = { "yamlfmt" },
