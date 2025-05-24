471b35606037f792db16840d514c4bbce43ef749
Christoph Bunte <christophbunte@gmail.com>
2025-05-24 18:18:37 +0200

    tweak ghostty config

--- home/ghostty/ghostty.nix
+++ home/ghostty/ghostty.nix
@@ -4,6 +4,7 @@
   programs.ghostty = {
     enable = true;
     settings = {
+      gtk-titlebar-hide-when-maximized = true;
       macos-option-as-alt = true;
     };
     package = if pkgs.stdenv.hostPlatform.isDarwin then null else pkgs.ghostty; # currently marked broken on Darwin
