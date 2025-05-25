ca3e61a49fea21c4362756dc024f5cf77508db31
Christoph Bunte <christophbunte@gmail.com>
2025-05-25 11:49:57 +0200

    virtualbox tweaks

--- system/configuration-virtualbox-guest.nix
+++ system/configuration-virtualbox-guest.nix
@@ -7,5 +7,7 @@
   disko.devices.disk.disk1.device = "/dev/sda";
 
   virtualisation.virtualbox.guest.enable = true;
+  virtualisation.virtualbox.guest.clipboard = true;
+  virtualisation.virtualbox.guest.dragAndDrop = false;
 
 }
