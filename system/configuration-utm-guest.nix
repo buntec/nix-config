50c111d3b0e0ebd11c46a4c6351da72f1ddb946d
Christoph Bunte <christophbunte@gmail.com>
2025-07-24 10:36:23 +0200

    fix share mount in UTM guest

--- system/configuration-utm-guest.nix
+++ system/configuration-utm-guest.nix
@@ -13,12 +13,28 @@
   # TODO: is this really needed?
   environment.variables.LIBGL_ALWAYS_SOFTWARE = "1";
 
-  # Mount shared folder
-  fileSystems."/mnt/host" = {
+  # Mount shared folder using virtfs
+  # https://docs.getutm.app/guest-support/linux/#virtfs
+  fileSystems."/mnt/share/from-utm" = {
     device = "share";
-    fsType = "virtiofs";
+    fsType = "9p";
     options = [
+      "trans=virtio"
+      "version=9p2000.L"
       "rw"
+      "_netdev"
+      "nofail"
+    ];
+  };
+
+  # this fixes permission issues
+  fileSystems."/mnt/host" = {
+    device = "/mnt/share/from-utm";
+    depends = [ "/mnt/share/from-utm" ];
+    fsType = "fuse.bindfs";
+    # https://docs.getutm.app/guest-support/linux/#fixing-permission-errors
+    options = [
+      "map=501/1000:@20/@1000"
       "nofail"
     ];
   };
