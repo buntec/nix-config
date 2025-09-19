2371b9fb7cb3a19b6efccc5459a070ef3476e5d4
Christoph Bunte <christophbunte@gmail.com>
2025-09-19 11:57:38 +0200

    add virtiofs based shared mount for UTM guests using Apple virtualization

--- system/configuration-utm-guest.nix
+++ system/configuration-utm-guest.nix
@@ -13,30 +13,43 @@
   # TODO: is this really needed?
   environment.variables.LIBGL_ALWAYS_SOFTWARE = "1";
 
-  # Mount shared folder using virtfs
-  # https://docs.getutm.app/guest-support/linux/#virtfs
-  fileSystems."/mnt/share/from-utm" = {
-    device = "share";
-    fsType = "9p";
-    options = [
-      "trans=virtio"
-      "version=9p2000.L"
-      "rw"
-      "_netdev"
-      "nofail"
-    ];
-  };
+  fileSystems = {
+    # VirtioFS shared folder (Apple Virtualization)
+    "/mnt/host/apple" = {
+      fsType = "virtiofs";
+      device = "share";
+      options = [
+        "rw"
+        "nofail"
+      ];
+    };
 
-  # this fixes permission issues
-  fileSystems."/mnt/host" = {
-    device = "/mnt/share/from-utm";
-    depends = [ "/mnt/share/from-utm" ];
-    fsType = "fuse.bindfs";
-    # https://docs.getutm.app/guest-support/linux/#fixing-permission-errors
-    options = [
-      "map=501/1000:@20/@1000"
-      "nofail"
-    ];
+    # Mount shared folder using virtfs (QEMU)
+    # https://docs.getutm.app/guest-support/linux/#virtfs
+    "/mnt/share/from-utm" = {
+      device = "share";
+      fsType = "9p";
+      options = [
+        "trans=virtio"
+        "version=9p2000.L"
+        "rw"
+        "_netdev"
+        "nofail"
+      ];
+    };
+
+    # this fixes permission issues
+    "/mnt/host/qemu" = {
+      device = "/mnt/share/from-utm";
+      depends = [ "/mnt/share/from-utm" ];
+      fsType = "fuse.bindfs";
+      # https://docs.getutm.app/guest-support/linux/#fixing-permission-errors
+      options = [
+        "map=501/1000:@20/@1000"
+        "nofail"
+      ];
+    };
+
   };
 
 }
