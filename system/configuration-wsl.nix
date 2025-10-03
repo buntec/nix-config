8ad0ee015638f641e8a5167c5d68938da715f9f7
Christoph Bunte <christophbunte@gmail.com>
2025-10-03 16:31:23 +0200

    wsl tweak

--- system/configuration-wsl.nix
+++ system/configuration-wsl.nix
@@ -6,8 +6,11 @@
 }:
 {
   wsl.enable = true;
+  wsl.useWindowsDriver = true; # OpenGL support - crucial for GUI apps!
+
   # changing the default username is too cumbersome:
   # https://nix-community.github.io/NixOS-WSL/how-to/change-username.html
   # wsl.defaultUser = machine.user;
+
   networking.hostName = "wsl";
 }
