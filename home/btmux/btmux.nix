3183c348552f8263e496a744ef4552bf2415870a
Christoph Bunte <christophbunte@gmail.com>
2026-08-13 09:36:17 +0300

    tweak btmux config

--- home/btmux/btmux.nix
+++ home/btmux/btmux.nix
@@ -4,14 +4,29 @@
     prefix = "C-a"
     shell = "${pkgs.fish}/bin/fish"
     vi-mode = true
-    wallpaper = "~/Downloads/note-thanun-KKuX3OhsfoE-unsplash.jpg"
+    animations = true
+    wallpaper = "https://images.unsplash.com/photo-1774998861301-5acc6fdfcf63"
     wallpaper-opacity = 0.025
     session-sort = "mru"
+    wallpaper-saturate = 0.5
     window-grid-count = 4
+    window-sort = "alphabetical"
+    show-pane-titles = false
+    pane-switch-intensity = 0.25
+    pane-switch-duration = 0.5
 
     [terminal]
+    cursor-blink = true
+    cursor-style = "bar"
     font-size = 18.0
     font-family = "Geist Mono"
     font-weight = 400
+    renderer = "webgl"
+    scrollback = 100000
+    scroll-sensitivity = 5.0
+
+    [log]
+    console-level = "warn"
+    file-level = "info"
   '';
 }
