5f882c7b3731aa4a26adfe91daaebc6c530d2cc2
Christoph Bunte <christophbunte@gmail.com>
2026-09-02 10:29:33 +0200

    btmux config tweak

--- home/btmux/btmux.nix
+++ home/btmux/btmux.nix
@@ -1,8 +1,9 @@
-{ pkgs, ... }:
+{ pkgs, mode, ... }:
 {
   xdg.configFile."btmux/config.toml".text = ''
     prefix = "C-a"
     shell = "${pkgs.fish}/bin/fish"
     vi-mode = true
+    colors = "https://raw.githubusercontent.com/buntec/kauz/refs/heads/main/base24/kauz-${mode}.yml"
   '';
 }
